da2645edf5a61347b4f528917e3961b42124c5d8
Abigail Addae <abigail2addae@gmail.com>
2022-06-08 22:36:40 +0000

    modularized connect function and moved to new file client.js

--- play.js
+++ play.js
@@ -1,23 +1,3 @@
-const net = require("net");
+const conn = require("./client");
 
-//establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541',
-  });
-
-  //interpret incoming data as text
-  conn.setEncoding("utf-8");
-
-  return conn;
-};
-
-connect()
-.on("connect", () => {
-  console.log("You have successfully connected");
-})
-.on("data", (data) => {
-  console.log(data);
-})
-
+conn.connect();
