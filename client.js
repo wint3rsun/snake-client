08a36503773a7ab32843bea83ba2dacab600b385
Abigail Addae <abigail2addae@gmail.com>
2022-06-08 23:19:43 +0000

    played around with connect function and cleaned up code a but

--- client.js
+++ client.js
@@ -1,18 +1,17 @@
 const net = require("net");
 
-//establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: '50541',
   });
 
-  //interpret incoming data as text
   conn.setEncoding("utf-8");
 
-  conn.on("connect", (client) => {
+  conn
+  .on("connect", () => {
     console.log("You have successfully connected to the game server");
-    conn.write("Name: AAA");
+    conn.write("Name: AAA");  
   })
   .on("data", (data) => {
     console.log(data);
