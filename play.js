84da9b351e2177df1a40ce9266075d02f213947c
Abigail Addae <abigail2addae@gmail.com>
2022-06-09 00:15:16 +0000

    moved setupInput and handUserInput functions to input.js and modularized play.js

--- play.js
+++ play.js
@@ -1,25 +1,6 @@
 const { stdin } = require("process");
-const conn = require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-conn.connect();
-
-//setup interface to handle user input from stdin
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf-8");
-  stdin.resume();
-  
-  stdin.on("data", handleUserInput);
-  
-  return stdin;
-};
-
+connect();
 setupInput();
